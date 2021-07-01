b5db9dc7b0115c9cad50f928cdb3d66082141a5f
thuve92 <72792411+thuve92@users.noreply.github.com>
2021-07-01 14:48:35 +0200

    navbar updates

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,16 +3,52 @@
     <div class="column">
       <div class="navbar is-transparent">
         <div class="navbar-brand">
-          <img :src="link" alt="loading" />
+          <img :src="logo" alt="loading" />
         </div>
-        <div class="column"></div>
-        <div class="navbar-start">
-          <div class="navbar-item">Hjem</div>
-          <div class="navbar-item">Ukens tilbud</div>
-          <div class="navbar-item">Gavekort</div>
-          <div class="navbar-item">Kundestøtte</div>
-          <div class="navbar-end">
-            <div class="navbar-item">Handlekurv</div>
+        <div class="column">
+          <div class="navbar-start">
+            <div class="navbar-item">
+              <span class="icon-text">
+                <span class="icon">
+                  <i class="fas fa-home"></i>
+                </span>
+                <span><a>Home</a></span>
+              </span>
+            </div>
+            <div class="navbar-item">
+              <span class="icon-text">
+                <span class="icon">
+                  <i class="fas fa-percent"></i>
+                </span>
+                <span><a>Ukens tilbud</a></span>
+              </span>
+            </div>
+            <div class="navbar-item">
+              <span class="icon-text">
+                <span class="icon">
+                  <i class="fas fa-gift-card"></i>
+                </span>
+                <span><a>Gavekort</a></span>
+              </span>
+            </div>
+            <div class="navbar-item">
+              <span class="icon-text">
+                <span class="icon">
+                  <i class="fas fa-user-headset"></i>
+                </span>
+                <span><a>Kundestøtte</a></span>
+              </span>
+            </div>
+            <div class="navbar-end">
+              <div class="navbar-item">
+                <span class="icon-text">
+                  <span class="icon">
+                    <i class="fas fa-shopping-cart"></i>
+                  </span>
+                  <span><a>Handlekurv</a></span>
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -25,7 +61,8 @@ export default {
   name: "NavBar",
   data() {
     return {
-      link: "https://i.ibb.co/RbgFzn6/logo.png",
+      logo: "https://i.ibb.co/3WMLcjh/logo.png",
+      links: {},
     };
   },
   props: {
@@ -36,9 +73,22 @@ export default {
 
 
 <style scoped>
+.navbar-brand {
+  width: 400px;
+  height: auto;
+}
 .navbar-start {
-  margin-left: 25vw;
+  margin-left: 15vw;
   font-size: 1.5em;
-  margin-top: 2.1rem;
+  margin-top: 4rem;
+}
+.navbar-item {
+  margin-left: 3rem;
+}
+.icon {
+  margin-top: -0.2rem;
+}
+.navbar a {
+  color: black;
 }
 </style>
