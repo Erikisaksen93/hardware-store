48bf6d4c39a1733e8955ff0e455aaa8e64cafdd8
Erik Isaksen <72092585+Erikisaksen93@users.noreply.github.com>
2021-07-02 12:44:04 +0200

    asdf

--- src/shared/data.js
+++ src/shared/data.js
@@ -1,41 +1,48 @@
 export const model = {
     datakomponenter: [
         {
-            prosessor:
-            {
-                navn: "Intel Core i17-19900KXX Prosessor",
-                beskrivelse: "Socket-LGA9000x, 69-Core, 420-Thread, 9.5/16.2GHz, Rocket Lake, uten kjøler",
-                img: "https://www.komplett.no/img/p/200/1180155.jpg",
-            },
+            type: "prosessor",
+            navn: "Intel Core i17-19900KXX Prosessor",
+            beskrivelse: "Socket-LGA9000x, 69-Core, 420-Thread, 9.5/16.2GHz, Rocket Lake, uten kjøler",
+            pris: 99000,
+            img: "https://www.komplett.no/img/p/200/1180155.jpg",
 
-            skjermkort:
-            {
-                navn: "Nvidia",
-                modell: "ASUS Geforce 3080ti ROG STRIX OC",
-                img: "https://www.komplett.no/img/p/200/1187638.jpg",
-            },
+        },
 
-            minne:
-            {
-                navn: "HyperX Fury RGB DDR4",
-                modell: "3200MHZ 32GB",
-                img: "https://www.komplett.no/img/p/200/1139729.jpg",
-            },
 
-            lagring:
-            {
-                navn: "WD BLACK",
-                modell: "SN850 1TB M.2 SSD",
-                img: "https://www.komplett.no/img/p/200/1173105.jpg",
-            },
+        {
+            type: "skjermkort",
+            navn: "MSI GeForce RTX3080Ti SUPRIM X",
+            beskrivelse: "Skjermkort, PCI-Express 4.0, 12GB GDDR6X, Ampere",
+            pris: 125500,
+            img: "https://www.komplett.no/img/p/200/1187638.jpg",
+        },
 
-            strøm:
-            {
-                navn: "Corsair",
-                modell: "RM850 - 850W PSU",
-                img: "https://www.komplett.no/img/p/200/1097976.jpg",
-            },
 
+        {
+            type: "minne",
+            navn: "HyperX Fury RGB DDR4 3200MHz 32GB",
+            beskrivelse: "4x8GB 3200MHz DDR4 CL18, XMP",
+            pris: 17000,
+            img: "https://www.komplett.no/img/p/200/1139729.jpg",
+        },
+
+
+        {
+            type: "lagring",
+            navn: "WD Black SN850 2TB M.2 SSD",
+            beskrivelse: "M.2 2280, PCIe 4.0 x4, NVMe, 7000/5100MB/s read/write",
+            pris: 16850,
+            img: "https://www.komplett.no/img/p/200/1173105.jpg",
+        },
+
+
+        {
+            type: "strøm",
+            navn: "Corsair HX850, 850W PSU",
+            beskrivelse: "ATX 12V V2.4, 80 PLUS Platinum, Modular, 6x 6+2-pin PCIe, 12x SATA, 8x Molex",
+            pris: 7650,
+            img: "https://www.komplett.no/img/p/200/1097976.jpg",
         },
     ],
     TV: [
