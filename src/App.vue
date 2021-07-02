b0d11a3830dd84bdaf8d6567247788aacc1230a4
Erik Isaksen <72092585+Erikisaksen93@users.noreply.github.com>
2021-07-02 10:53:50 +0200

    asdf

--- src/App.vue
+++ src/App.vue
@@ -41,58 +41,7 @@ export default {
 
   data() {
     return {
-      model: {
-        datakomponenter: [
-          {
-            prosessor: [
-              {
-                navn: "Intel",
-                modell: "i9-11900k",
-              },
-            ],
-            skjermkort: [
-              {
-                navn: "Nvidia",
-                modell: "ASUS Geforce 3080ti ROG STRIX OC",
-              },
-            ],
-            minne: [
-              {
-                navn: "HyperX Fury RGB DDR4",
-                modell: "3200MHZ 32GB",
-              },
-            ],
-            lagring: [
-              {
-                navn: "WD BLACK",
-                modell: "SN850 1TB M.2 SSD",
-              },
-            ],
-            strøm: [
-              {
-                navn: "Corsair",
-                modell: "RM850 - 850W PSU",
-              },
-            ],
-          },
-        ],
-        TV: [
-          {
-            LG: [
-              {
-                navn: `LG 65" 4K NanoCell Smart TV`,
-                modell: "65NANO81",
-              },
-            ],
-            samsung: [
-              {
-                navn: `Samsung 85" QLED 4K Smart TV`,
-                modell: "QE85Q60",
-              },
-            ],
-          },
-        ],
-      },
+      datakomponenter: [],
     };
   },
 };
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,37 +1,47 @@
 <template>
   <div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
-    <div>Test</div>
+    <div class="section">
+      <div class="columns">
+        <div class="column is-one-fifth">
+          <img :src="datakomponenter[0].prosessor[0].img" />
+        </div>
+        <div class="column is-three-fifth">
+          <p>Info</p>
+        </div>
+        <div class="column is-one-fifth">
+          <p>Pris:</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { model } from "../shared/data";
+import deepClone from "deep-clone";
+
 export default {
   name: "MainPage",
   props: {
     msg: String,
   },
+  created() {
+    this.datakomponenter = deepClone(model.datakomponenter);
+    this.tv = deepClone(model.TV);
+  },
+
+  data() {
+    return {
+      datakomponenter: null,
+      tv: null,
+    };
+  },
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+img {
+  width: 1200px;
+  height: auto;
+}
 </style>
